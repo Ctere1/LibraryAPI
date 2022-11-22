8267eb688e0c0fa61f3fe5dffb1bb9231087a068
Cemil Tan <cemiltan1999@gmail.com>
2022-11-22 20:33:29 +0300

    Auth: files added

---
+++ LibraryAPI/App_Start/Startup.cs
@@ -0,0 +1,42 @@
+﻿using LibraryAPI.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(LibraryAPI.App_Start.Startup))]
+
+namespace LibraryAPI.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            HttpConfiguration httpConfiguration = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+            WebApiConfig.Register(httpConfiguration);
+            app.UseWebApi(httpConfiguration);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions()
+            {
+                TokenEndpointPath = new Microsoft.Owin.PathString("/api/Auth/GetToken"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(15),
+                AllowInsecureHttp = true,
+                Provider = new AuthorizationServerProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(options);
+
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+    }
+}
---
+++ LibraryAPI/Providers/AuthorizationServerProvider.cs
@@ -0,0 +1,50 @@
+﻿using LibraryAPI.Models;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LibraryAPI.Providers
+{
+    public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            //Get admin's and user's credentials 
+            List<admin> adminList = new List<admin>();
+            List<user> userList = new List<user>();
+            libraryManagementEntities db = new libraryManagementEntities();
+            adminList = db.admins.ToList();
+            userList = db.users.ToList();
+
+            if (adminList.Exists(x => x.name == context.UserName) && adminList.Exists(x => x.password == context.Password))
+            {
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                context.Validated(identity);
+            }
+            else if (userList.Exists(x => x.name == context.UserName) && userList.Exists(x => x.password == context.Password))
+            {
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "Username or password incorrect");
+            }
+        }
+    }
+}
