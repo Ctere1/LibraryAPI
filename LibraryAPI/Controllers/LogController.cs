e7a1fe7d13a1ac71a7839439c6d09c437de0fb08
Cemil Tan <cemiltan1999@gmail.com>
2022-12-18 19:06:42 +0300

    Log and Signup controllers added

---
+++ LibraryAPI/Controllers/LogController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using LibraryAPI.Models;
+
+namespace LibraryAPI.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class LogController : ApiController
+    {
+        private libraryManagementEntities db = new libraryManagementEntities();
+
+        // GET: api/Log
+        public IQueryable<log> Getlogs()
+        {
+            return db.logs;
+        }
+
+        // GET: api/Log/5
+        [ResponseType(typeof(log))]
+        public IHttpActionResult Getlog(int id)
+        {
+            log log = db.logs.Find(id);
+            if (log == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(log);
+        }
+
+        // DELETE: api/Log/5
+        [ResponseType(typeof(log))]
+        public IHttpActionResult Deletelog(int id)
+        {
+            log log = db.logs.Find(id);
+            if (log == null)
+            {
+                return NotFound();
+            }
+
+            db.logs.Remove(log);
+            db.SaveChanges();
+
+            return Content(HttpStatusCode.OK, "Log deleted");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
---
+++ LibraryAPI/Controllers/SignupController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using LibraryAPI.Models;
+
+namespace LibraryAPI.Controllers
+{
+    public class SignupController : ApiController
+    {
+        private libraryManagementEntities db = new libraryManagementEntities();
+
+        // POST: api/Signup
+        [ResponseType(typeof(user))]
+        public IHttpActionResult Signup(user user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userInDb = db.users.Any(x => x.email == user.email);
+            if (!userInDb)
+            {
+                db.users.Add(user);
+                db.SaveChanges();
+                return Content(HttpStatusCode.OK, "New user created. You can access API with your credentials");
+            }
+            else
+            {
+                return BadRequest("User already exists. Change the user email and try again");
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
